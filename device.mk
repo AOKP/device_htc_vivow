5f6c3baed521e54a2b1865b7936f0fd92978cac3
Evan McClain <aeroevan@gmail.com>
2011-12-26 20:26:16 -0500

    Put bcm4329.hcd in the right spot...
    Signed-off-by: Evan McClain <aeroevan@gmail.com>

--- device.mk
+++ device.mk
@@ -113,7 +113,7 @@ PRODUCT_COPY_FILES += \
 
 # Firmware
 PRODUCT_COPY_FILES += \
-    device/htc/vivow/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+    device/htc/vivow/firmware/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd \
     device/htc/vivow/firmware/default.acdb:system/etc/firmware/default.acdb \
     device/htc/vivow/firmware/default_mfg.acdb:system/etc/firmware/default_mfg.acdb \
     device/htc/vivow/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
