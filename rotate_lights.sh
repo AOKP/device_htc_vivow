3d3195bcb03523868d992be35c47d95e5b275ba7
Matt Stockwell <matt.stockwell@optusnet.com.au>
2012-01-05 17:38:16 +1000

    Softkey Rotation: Fix rotate lights helper script

--- rotate_lights.sh
+++ rotate_lights.sh
@@ -2,16 +2,17 @@
 #Softkey Rotation helper script
 #By St.Matt
 
-if [ $1 -eq 1 ] #lights in landscape mode
+if [ $1 -eq 1 ] #lights in landscape
 then
     brightness=`cat /sys/class/leds/button-backlight-portrait/brightness`
-    if [ $brightness -ne 0 ] #Prevent issue when going from landscape 90d to 270d
-    then
-        echo $brightness > /sys/class/leds/button-backlight-landscape/brightness
-        echo 0 > /sys/class/leds/button-backlight-portrait/brightness
-    fi
-else #lights in portrait mode
+    echo $brightness > /sys/class/leds/button-backlight-landscape/brightness
+    echo 0 > /sys/class/leds/button-backlight-portrait/brightness
+
+else #lights in portrait
     brightness=`cat /sys/class/leds/button-backlight-landscape/brightness`
-    echo $brightness > /sys/class/leds/button-backlight-portrait/brightness
-    echo 0 > /sys/class/leds/button-backlight-landscape/brightness
+    if [ $brightness -ne 0 ] #Prevent issue when going from 0d to 270d
+    then
+        echo $brightness > /sys/class/leds/button-backlight-portrait/brightness
+        echo 0 > /sys/class/leds/button-backlight-landscape/brightness
+    fi
 fi
