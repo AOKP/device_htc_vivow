808f67f2cc254dcd9f371bc26fc930e5e6a6d442
fzservers@gmail.com <fzservers@gmail.com>
2012-04-02 09:18:31 -0400

    Rekang aeroevan and then shameless renamings

---
+++ aokp.mk
@@ -0,0 +1,13 @@
+PRODUCT_RELEASE_NAME := Inc2
+
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+$(call inherit-product, vendor/aokp/configs/common-versions.mk)
+$(call inherit-product, vendor/aokp/products/vivow.mk)
+
+$(call inherit-product, device/htc/vivow/full_vivow.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivow BUILD_ID=IML74K BUILD_DISPLAY_ID=IML74K BUILD_FINGERPRINT=verizon_wwe/htc_vivow/vivow:2.3.4/GRJ22/120826.3:user/release-keys PRIVATE_BUILD_DESC="4.08.605.3 CL120826 release-keys"
+
+PRODUCT_NAME := aokp_vivow
+PRODUCT_DEVICE := vivow
